e7ef510baaf9e5a0425557efe2fdeeeb54731ae8
Nicholas Quezada <nicholas.quezada@outlook.com>
2025-02-18 21:33:07 -0700

    feat: add basic TCP server implementation
    - Introduced a TCP server that listens on 0.0.0.0:6379.
    - Added handling for incoming connections and reading data.
    - Responds with a simple '+OK' message to clients.
    - Created Cargo.lock for dependency management.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::str;
+use std::{
+    io::{BufReader, Read, Write},
+    net,
+};
+
+const ADDR: &str = "0.0.0.0:6379";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = net::TcpListener::bind(ADDR).unwrap();
+
+    println!("Listening on {}", ADDR);
+
+    for stream in listener.incoming() {
+        println!("Connection established");
+        let mut stream = stream.unwrap();
+        let mut reader = BufReader::new(&stream);
+        let mut buf = vec![];
+        let count = reader.read(&mut buf).unwrap();
+        let content = str::from_utf8(&buf[0..count]).unwrap();
+        println!("{}", content);
+        stream.write_all("+OK\r\n".as_bytes()).unwrap();
+    }
 }
